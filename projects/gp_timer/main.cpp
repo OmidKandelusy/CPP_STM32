cae340f9139b442da042fd7388897fe39bbf1cef
Omid Kandelusy <omidkandelusy@gmail.com>
2026-04-29 21:30:33 +0200

    feat: updates the project for the gtimer driver

--- projects/gp_timer/main.cpp
+++ projects/gp_timer/main.cpp
@@ -5,22 +5,44 @@
 
 /** driver header files */
 #include "gtimer.h"
+#include "wait.h"
 
+/** subsystem header files */
+#include "blink.hpp"
 
+#define PIN_NUMBER 5
+
+static volatile uint8_t timeout_flag = 0;
 // ===============================================================================
 // main routine
 
 void timeout_callback(void){
-    // do nothing yet
+    timeout_flag = 1;
 }
 
 int main(void){
     int ret = 0;
+
+    // creating a blink object:
+    Blink Blink(GPIOA, PIN_NUMBER);
+
+    // initialize the object:
+    Blink.init();
     
     gtimer_init(timeout_callback);
 
     ret = gtimer_timeout_ms(500);
-    if (ret <0 ) while (1);
+    if (ret <0 ){
+        Blink.blink();
+    }
+
+    while(1){
+        sys_wait_ms(400);
+
+        if (timeout_flag){
+            Blink.pattern(PATTERN_1);
+        }
+    }
 
     return 0;
 }
